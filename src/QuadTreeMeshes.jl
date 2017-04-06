b22612b257190c06a78c03e0a5fefefcdc1e2199
Matthias Vigelius <mvigelius@gmail.com>
2017-04-06 08:08:17 +0200

    added subdivision with constraint

--- src/QuadTreeMeshes.jl
+++ src/QuadTreeMeshes.jl
@@ -1,5 +1,8 @@
 module QuadTreeMeshes
 
+import GeometryTypes
+
 # package code goes here
+include("QuadTree.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using QuadTreeMeshes
 using Base.Test
+import GeometryTypes
 
-# write your own tests here
-@test 1 == 2
+include("TestHelpers.jl")
+include("QuadTreeTests.jl")
